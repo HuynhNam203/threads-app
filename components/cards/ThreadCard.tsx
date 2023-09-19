2c32cd9a8fbe8a8e5d98c39b044616898f848657
HuynhNam203 <namlh07927692@gmail.com>
2023-09-19 15:55:58 +0700

    new udpate 19/9

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -67,7 +67,7 @@ const ThreadCard = ({
 
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-            <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
+            <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
               <div className="flex gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
@@ -114,15 +114,24 @@ const ThreadCard = ({
         {/* {TODO: DeleteThread} */}
         {/* {TODO: Show comment logos} */}
         {!isComment && community && (
-          <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
-            <p className='text-subtle-medium text-gray-1'>
+        <Link
+          href={`/communities/${community.id}`}
+          className='mt-5 flex items-center'
+        >
+          <p className='text-subtle-medium text-gray-1'>
             {formatDateString(createdAt)}
             {community && ` - ${community.name} Community`}
           </p>
 
-            <Image src={community.image} alt={community.name} width={24} height={24} className="ml-1 rounded-full object-cover"/>
-          </Link>
-        )}
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={14}
+            height={14}
+            className='ml-1 rounded-full object-cover'
+          />
+        </Link>
+      )}
       </div>
     </article>
   );
